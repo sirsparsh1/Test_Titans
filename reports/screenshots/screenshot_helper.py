04c69b4776dbbf4b63ebfc98fc38960be7572f28
sirsparsh1 <sirsparsh1@gmail.com>
2026-05-22 18:54:59 +0530

    push code

--- reports/screenshots/screenshot_helper.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def take_screenshot(driver, scenario_name):
-
-    path = "reports/screenshots"
-    os.makedirs(path, exist_ok=True)
-
-    driver.save_screenshot(f"{path}/{scenario_name}.png")
